700bc1fabab31fc50cf13963fce2406b8f8af221
Reboot <sergejskorikjr@gmail.com>
2023-03-07 08:26:21 +0200

    Added SQL support to load posts, etc. Added functionality to create, update an delete posts

--- main.py
+++ main.py
@@ -1,19 +1,82 @@
-from flask import Flask, render_template, request
+import os
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
-import requests
+from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
 import myforms
+import bleach  # To clean user input when create post
 
 app = Flask(__name__)
-app.secret_key = "fdffdjj45tktykh"
+app.config["SECRET_KEY"] = "fdffdjj45tktykh"
+ckeditor = CKEditor(app)
 Bootstrap(app)
 
-url = "https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json"
-posts = requests.get(url).json()
+# Connect to DB
+db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+def strip_invalid_html(content):
+    """Strips invalid tags/attributes"""
+    allowed_tags = frozenset(('a', 'abbr', 'acronym', 'address', 'b', 'br', 'div', 'dl', 'dt',
+                              'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
+                              'li', 'ol', 'p', 'pre', 'q', 's', 'small', 'strike',
+                              'span', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
+                              'thead', 'tr', 'tt', 'u', 'ul'))
+
+    allowed_attrs = {
+        'a': frozenset(('href', 'target', 'title')),
+        'img': frozenset(('src', 'alt', 'width', 'height')),
+    }
+
+    cleaned = bleach.clean(content,
+                           tags=allowed_tags,
+                           attributes=allowed_attrs,
+                           strip=True)
+
+    return cleaned
 
 
 @app.route("/")
 def homepage():
+    # Get last 5 posts
+    posts = db.session.query(BlogPost).order_by(BlogPost.id.desc()).limit(5)
+    posts = posts[::-1]
     return render_template("index.html", all_posts=posts)
+
+
+@app.route("/new-post", methods=["GET", "POST"])
+def create_post():
+    form = myforms.CreatePostForm()
+    if form.validate_on_submit():
+        all_post_data = {item: strip_invalid_html(value.data) for item, value in form._fields.items() if
+                         item not in ['submit', 'csrf_token']}
+        current_date = datetime.now().strftime("%B %d, %Y")
+        # cleaned_post_body = strip_invalid_html(form.body.data)
+
+        new_post = BlogPost(date=current_date, **all_post_data)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('homepage'))
+    return render_template("make_post.html", form=form)
 
 
 @app.route("/about")
@@ -23,22 +86,49 @@ def about():
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == post_id:
-            requested_post = blog_post
-            break
+    requested_post = BlogPost.query.get(post_id)
+
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    edit_form = myforms.CreatePostForm(
+        title=requested_post.title,
+        subtitle=requested_post.subtitle,
+        img_url=requested_post.img_url,
+        author=requested_post.author,
+        body=requested_post.body
+    )
+    if edit_form.validate_on_submit():
+        # Changing requested_post info to what user wrote in edit_form
+        [setattr(requested_post, field.name, strip_invalid_html(field.data)) for field in edit_form if
+         field.name not in ['submit', 'csrf_token']]
+
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=requested_post.id))
+
+    return render_template("make_post.html", form=edit_form, is_edit=True)
+
+
+@app.route("/delete-post/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+
+    return redirect(url_for('homepage'))
 
 
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
-    if request.method == "POST":
-        data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["message"])
-        return render_template("contact.html", msg_sent=True)
+    # if request.method == "POST":
+    #     data = request.form
+    #     print(data["name"])
+    #     print(data["email"])
+    #     print(data["message"])
+    #     return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
 
@@ -54,5 +144,4 @@ def login():
 
 
 if __name__ == '__main__':
-    # print(posts[0].keys())
     app.run(debug=True)
--- myforms.py
+++ myforms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
 import email_validator
 
 
@@ -13,3 +14,12 @@ class ContactForm(FlaskForm):
         validators.Length(min=4), DataRequired()
     ])
     submit = SubmitField(label="Log in")
+
+
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
