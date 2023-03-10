7692ed7f2e12efe2137c98f3be24ee328cb817bb
Reboot <sergejskorikjr@gmail.com>
2023-03-10 08:24:07 +0200

    Improved Login system using flask-login module. Added relationship between User and Post author in database

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
 from datetime import datetime
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_gravatar import Gravatar
+from functools import wraps
 import myforms
 import bleach  # To clean user input when create post
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "fdffdjj45tktykh"
+app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -17,17 +22,38 @@ Bootstrap(app)
 db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'posts.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
+    author = relationship("User", back_populates="posts")  # db.Column(db.String(250), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100), unique=True)
+
+    # This will act like a List of BlogPost objects attached to each User.
+    # The "author" refers to the author property in the BlogPost class.
+    posts = relationship("BlogPost", back_populates="author")
+
 
 
 with app.app_context():
@@ -55,15 +81,32 @@ def strip_invalid_html(content):
     return cleaned
 
 
+def admin_only(func):
+    """My superuser decorator to make sure user with id=1 is admin"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if current_user.id != 1:
+            return abort(403)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@login_manager.user_loader
+def load_user(user_id: int):
+    return db.session.get(User, user_id)
+
+
 @app.route("/")
 def homepage():
     # Get last 5 posts
     posts = db.session.query(BlogPost).order_by(BlogPost.id.desc()).limit(5)
     posts = posts[::-1]
-    return render_template("index.html", all_posts=posts)
+    return render_template("index.html", all_posts=posts, logged_in=current_user.is_authenticated)
 
 
 @app.route("/new-post", methods=["GET", "POST"])
+@login_required
+@admin_only
 def create_post():
     form = myforms.CreatePostForm()
     if form.validate_on_submit():
@@ -92,6 +135,8 @@ def show_post(post_id):
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@login_required
+@admin_only
 def edit_post(post_id):
     requested_post = BlogPost.query.get(post_id)
     edit_form = myforms.CreatePostForm(
@@ -113,6 +158,8 @@ def edit_post(post_id):
 
 
 @app.route("/delete-post/<int:post_id>")
+@login_required
+@admin_only
 def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
@@ -129,18 +176,60 @@ def contact():
     #     print(data["email"])
     #     print(data["message"])
     #     return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False)
+    return render_template("contact.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    register_form = myforms.RegisterForm()
+    if register_form.validate_on_submit():
+        # User already exists
+        if User.query.filter_by(email=request.form.get('email')).first():
+            flash("This email is already in use, try to log in")
+            return redirect(url_for("login"))
+
+        hashed_password = generate_password_hash(
+            register_form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+
+        new_user = User(
+            email=register_form.email.data,
+            password=hashed_password,
+            name=register_form.name.data
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(new_user)
+        return redirect(url_for("homepage"))
+    return render_template("register.html", form=register_form, logged_in=current_user.is_authenticated)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    login_form = myforms.ContactForm()
-    if request.method == "POST" and login_form.validate_on_submit():
-        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
-            return render_template("success_login.html")
-        print(login_form.email.data)
-        return render_template('denied_login.html')
-    return render_template('login.html', form=login_form)
+    login_form = myforms.LoginForm()
+    if login_form.validate_on_submit():
+        password = login_form.password.data
+        user = User.query.filter_by(email=login_form.email.data).first()
+
+        if not user:
+            flash("There's no account with this email registered")
+        elif not check_password_hash(user.password, password):
+            flash("Wrong password, try again")
+        else:
+            login_user(user)
+            return redirect(url_for("homepage"))
+
+    return render_template("login.html", form=login_form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
 
 
 if __name__ == '__main__':
--- myforms.py
+++ myforms.py
@@ -5,17 +5,6 @@ from flask_ckeditor import CKEditorField
 import email_validator
 
 
-class ContactForm(FlaskForm):
-    email = StringField(label='Email', validators=[
-        validators.Length(min=6, message=u"email is too short"),
-        validators.Email(), DataRequired()
-    ])
-    password = PasswordField(label="Password", validators=[
-        validators.Length(min=4), DataRequired()
-    ])
-    submit = SubmitField(label="Log in")
-
-
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
@@ -23,3 +12,26 @@ class CreatePostForm(FlaskForm):
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Username", validators=[DataRequired()])
+    email = StringField(label='Email', validators=[
+        validators.Length(min=6, message=u"Email is too short"),
+        validators.Email(), DataRequired()
+    ])
+    password = PasswordField(label="Password", validators=[
+        validators.Length(min=4, message=u"Password has to be at least 4 characters long"), DataRequired()
+    ])
+    submit = SubmitField(label="Sign in")
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[
+        validators.Length(min=6, message=u"Email is too short"),
+        validators.Email(), DataRequired()
+    ])
+    password = PasswordField(label="Password", validators=[
+        validators.Length(min=4, message=u"Password has to be at least 4 characters long"), DataRequired()
+    ])
+    submit = SubmitField(label="Log in")
